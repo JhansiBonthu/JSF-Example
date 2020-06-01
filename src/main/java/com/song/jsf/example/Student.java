d186bb0d8be4f2356b043c1fffafefa48c65b901
JhansiBonthu <jhansi.bonthu26@gmail.com>
2020-06-01 12:04:07 +0200

    Added age field to the student object

--- src/main/java/com/song/jsf/example/Student.java
+++ src/main/java/com/song/jsf/example/Student.java
@@ -1,17 +1,22 @@
 package com.song.jsf.example;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Student implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	private static final Logger LOGGER = Logger.getLogger(Student.class.getName());
 	private Long id;
 	private String name;
+	private int age;
 
 	public Student() {}
-	public Student(Long id, String name) {
+	
+	public Student(Long id, String name,int age) {
 		this.id = id;
 		this.name = name;
+		this.age = age;
 	}
 
 	public Long getId() { return id; }
@@ -21,11 +26,19 @@ public class Student implements Serializable {
 
 	@Override
 	public Student clone() {
-		return new Student(id, name);
+		return new Student(id, name,age);
 	}
 
 	public void restore(Student student) {
 		this.id = student.getId();
 		this.name = student.getName();
+		this.age = student.getAge();
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
